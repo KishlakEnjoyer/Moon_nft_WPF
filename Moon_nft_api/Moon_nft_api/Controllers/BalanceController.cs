599fb626fcf1ec498ee384b54ba792aa448c2c9d
KishlakEnjoyer <erik.fayzullin.2007@mail.ru>
2025-09-25 19:50:22 +0500

    Сделал поплнение баланса для бота

---
+++ Moon_nft_api/Moon_nft_api/Controllers/BalanceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moon_nft_api.Models;
+using System.Data;
+using Dapper;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Moon_nft_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BalanceController : ControllerBase
+    {
+        
+
+        [HttpPost("topup")]
+        public async Task<IActionResult> TopUpBalance([FromBody] TopUpRequest request)
+        {
+            var currUser = MoonNftDbContext.GetContext.Users.FirstOrDefault(u => u.EmailUser == request.Email);
+            if (currUser is not null)
+            {
+                currUser.BalanceUser += request.Amount;
+                MoonNftDbContext.GetContext.SaveChanges();
+                return Ok($"Пополнение прошло успешно на {request.Amount}");
+            }
+            return BadRequest($"Не удалось пополнить баланс пользователю {request.Email}");
+        }
+    }
+
+   
+}
---
+++ Moon_nft_api/Moon_nft_api/Models/TopUpRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Moon_nft_api.Models
+{
+    public class TopUpRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public float Amount { get; set; }
+    }
+}
